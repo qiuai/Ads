a9be7bfbe7364fd0df3711df975f1c4f5d73cb4d
Vonwey <vonwey@qq.com>
2013-11-25 10:13:13 +0800

    公共函数建立

---
+++ Code/App/Admin/Lib/Action/PublicAction.class.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * 广告联盟系统 公共操作
+ * 
+ * @copyright (C)2012 ZHTS Inc.
+ * @project project_name
+ * @author Vonwey <VonweyWang@gmail.com>
+ * @CreateDate: 2013-11-25 上午9:57:14
+ * @version 1.0
+ *
+ * @ModificationHistory  
+ * Who          When                What 
+ * --------     ----------          ------------------------------------------------ 
+ * Vonwey   2013-11-25 上午9:57:14      todo
+ */
+class IndexAction extends CommonAction {
+	/**
+	 * 检测用户是否登录
+	 *
+	 * @author Vonwey <VonweyWang@gmail.com>
+	 * @CreateDate: 2013-11-25 上午10:10:26
+	 */
+	public function checkUser(){
+		
+	}
+	/**
+	 * 用户登录
+	 *
+	 * @author Vonwey <VonweyWang@gmail.com>
+	 * @CreateDate: 2013-11-25 上午10:10:51
+	 */
+	public function login(){
+		
+	}
+	/**
+	 * 用户登出
+	 *
+	 * @author Vonwey <VonweyWang@gmail.com>
+	 * @CreateDate: 2013-11-25 上午10:11:13
+	 */
+	public function logout(){
+		
+	}
+}
---
+++ Code/App/Ads/Lib/Action/PublicAction.class.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * 广告联盟系统  公共操作
+ * 
+ * @copyright (C)2012 ZHTS Inc.
+ * @project project_name
+ * @author Vonwey <VonweyWang@gmail.com>
+ * @CreateDate: 2013-11-25 上午10:07:57
+ * @version 1.0
+ *
+ * @ModificationHistory  
+ * Who          When                What 
+ * --------     ----------          ------------------------------------------------ 
+ * Vonwey   2013-11-25 上午10:07:57      todo
+ */
+class PublicAction extends CommonAction {
+	/**
+	 * 检测用户是否登录
+	 *
+	 * @author Vonwey <VonweyWang@gmail.com>
+	 * @CreateDate: 2013-11-25 上午10:10:26
+	 */
+	public function checkUser(){
+	
+	}
+	/**
+	 * 用户登录
+	 *
+	 * @author Vonwey <VonweyWang@gmail.com>
+	 * @CreateDate: 2013-11-25 上午10:10:51
+	 */
+	public function login(){
+	
+	}
+	/**
+	 * 用户登出
+	 *
+	 * @author Vonwey <VonweyWang@gmail.com>
+	 * @CreateDate: 2013-11-25 上午10:11:13
+	 */
+	public function logout(){
+	
+	}
+}
