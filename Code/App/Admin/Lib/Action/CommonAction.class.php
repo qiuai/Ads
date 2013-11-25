6ea6c81f599c55b164605ebf55f0f1b26e5c64a6
Vonwey <vonwey@qq.com>
2013-11-25 10:31:17 +0800

    CommonAction.class.php

---
+++ Code/App/Admin/Lib/Action/CommonAction.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 广告联盟系统 公共方法
+ * 
+ * @copyright (C)2012 ZHTS Inc.
+ * @project project_name
+ * @author Vonwey <VonweyWang@gmail.com>
+ * @CreateDate: 2013-11-25 上午9:56:28
+ * @version 1.0
+ *
+ * @ModificationHistory  
+ * Who          When                What 
+ * --------     ----------          ------------------------------------------------ 
+ * Vonwey   2013-11-25 上午9:56:28      todo
+ */
+class CommonAction extends Action {
+	/**
+	 * 构造函数
+	 *
+	 * @author Vonwey <VonweyWang@gmail.com>
+	 * @CreateDate: 2013-11-25 上午10:06:53
+	 */
+    function _initialize(){
+    	
+    }
+}
