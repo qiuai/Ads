09f2348d6170de97451592491040876012c00f58
Vonwey <vonwey@qq.com>
2013-11-25 09:05:40 +0800

    配置文件模板 统一

--- Code/App/Authority/Conf/domain.sample.php
+++ Code/App/Authority/Conf/domain.sample.php
@@ -3,5 +3,7 @@
 return array(
 	// 网站地址
 	'SITE_URL'		   => 'http://a.ads.me',
+
+	'__PUBLIC__'	   => 'http://s.ads.me/authority', // rabc 静态文件路径 转义
 );
 
--- Code/Conf/db.global.sample.php
+++ Code/Conf/db.global.sample.php
@@ -1,11 +1,12 @@
 <?php
 return array(
-	'DB_TYPE'	=> 'mysql',
-	'DB_HOST'	=> 'localhost',
-	'DB_NAME'	=> 'zhts_ads',
-	'DB_USER'	=> 'root',
-	'DB_PWD'	=> 'vonwey',
-	'DB_PORT'	=> '3306',
-	'DB_PREFIX'	=> 'zhts_',
+	//数据库配置信息
+	'DB_TYPE'   => 'mysql',		// 数据库类型
+	'DB_HOST'   => 'localhost', // 服务器地址
+	'DB_NAME'   => 'ads_zhts',	// 数据库名
+	'DB_USER'   => 'root',		// 用户名
+	'DB_PWD'    => 'vonwey',	// 密码
+	'DB_PORT'   => 3306,		// 端口
+	'DB_PREFIX' => 'zhts_',		// 数据库表前缀
 );
 ?>
