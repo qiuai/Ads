62dc7fd1fefa0e564a0a3f669cafd48ef5c8bd53
Vonwey <vonwey@qq.com>
2013-11-28 09:46:45 +0800

    后台 控制器 模板

---
+++ Code/App/Admin/Lib/Action/WebAction.class.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * 广告联盟系统  首页
+ * 
+ * @copyright (C)2012 ZHTS Inc.
+ * @project project_name
+ * @author Vonwey <VonweyWang@gmail.com>
+ * @CreateDate: 2013-11-25 上午9:58:45
+ * @version 1.0
+ *
+ * @ModificationHistory  
+ * Who          When                What 
+ * --------     ----------          ------------------------------------------------ 
+ * Vonwey   2013-11-25 上午9:58:45      todo
+ */
+class WebAction extends CommonAction {
+    public function index(){
+    	$this->display();
+	}
+	public function header(){
+		$this->display();
+	}
+	public function footer(){
+		$this->display();
+	}
+	public function left(){
+		$this->display();
+	}
+	public function right(){
+		$this->display();
+	}
+}
