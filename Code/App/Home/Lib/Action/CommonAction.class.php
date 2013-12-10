4abaf33f5247fe48961a4fbbffed4e6c9716b40a
Vonwey <vonwey@qq.com>
2013-12-10 11:25:51 +0800

    Uploadfile/index.html

---
+++ Code/App/Home/Lib/Action/CommonAction.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 广告联盟系统  前端  基类
+ * 
+ * @copyright (C)2012 ZHTS Inc.
+ * @project project_name
+ * @author Vonwey <VonweyWang@gmail.com>
+ * @CreateDate: 2013-12-10 上午11:10:56
+ * @version 1.0
+ *
+ * @ModificationHistory  
+ * Who          When                What 
+ * --------     ----------          ------------------------------------------------ 
+ * Vonwey   2013-12-10 上午11:10:56      todo
+ */
+class CommonAction extends Action {
+}
