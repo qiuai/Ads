c77febee76e8302768df6e1265e5e35c3c4e3dab
Vonwey <vonwey@qq.com>
2013-12-11 11:48:01 +0800

    adv config

--- Code/App/Home/Conf/domain.sample.php
+++ Code/App/Home/Conf/domain.sample.php
@@ -3,6 +3,12 @@
 return array(
 	// 网站地址
 	'SITE_URL'		   => 'http://www.ads.me/index.php',
+
+	// 网站主
+	'WEB_URL'		   => 'http://www.ads.me/web.php',
+
+	// 广告主
+	'ADV_URL'		   => 'http://www.ads.me/adv.php',
 	
 );
 
