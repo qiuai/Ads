fa97fb9a26fc93d212d5cc763cb5925a9144f45f
yumao <815227173@qq.com>
2013-12-12 18:12:56 +0800

    广告计划功能基本做完后

--- Code/App/Admin/Lib/Model/AdPlanModel.class.php
+++ Code/App/Admin/Lib/Model/AdPlanModel.class.php
@@ -6,12 +6,17 @@ class	AdPlanModel extends RelationModel{
 	
 	// 定义验证规则
 	protected $_validate = array(
-		array('uid','number','广告主uid必须为数字',1), // 	广告主id验证
+		array('uid','number','广告主uid必须为数字',1,'',3), // 	广告主id验证
 		array('jump_url','url','目标网址格式不正确',1), // 验证目标网址
 		array('plan_name','require','计划名称必须存在',1), // 验证目标网址
 			
 		array('plan_name','','计划名称不能重复',1,'unique',1),  // 计划名称必须唯一
 		
+	);
+	
+	// 定义自动完成规则
+	protected $_auto = array(
+		array('uid','intval',self::MODEL_BOTH,'function'),
 	);
 	
 	// 定义关联的相关规则
