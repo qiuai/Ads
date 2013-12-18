ab84e2de292eeb73012a54856dfe41e6f4eaf5dc
xieyihong <305095253@qq.com>
2013-12-18 18:09:54 +0800

    finance

--- Code/App/Admin/Lib/Action/FinanceAction.class.php
+++ Code/App/Admin/Lib/Action/FinanceAction.class.php
@@ -14,9 +14,8 @@
  * Vonwey   2013-11-25 上午9:58:45      todo
  */
 class FinanceAction extends CommonAction {
-	// 提现申请列表
+	//提现申请列表
     public function index(){
-		// 创建提现申请列表对象
 		$fa			= M("finance_apply");
 		$uid		= (int)($_POST["uid"]);
 		$status		= (int)($_GET["status"]);
@@ -24,7 +23,7 @@ class FinanceAction extends CommonAction {
 			if(empty($uid)){
 				$where  = "1";
 			}else{ 
-				$where  = "id=".$uid;
+				$where  = "uid=".$uid;
 			}
 		}else{	// 按状态查询
 			$where  = "status = ".$status;
@@ -32,10 +31,18 @@ class FinanceAction extends CommonAction {
 		$apply		= $fa->where($where)->select();
 		foreach($apply as $key =>$value){
 			$apply[$key]["apply_date"] = date("Y-m-d H:i:s",$value["apply_date"]);
-			$apply[$key]["process_time"] = date("Y-m-d H:i:s",$value["process_time"]);
+			$apply[$key]["process_time"] = date("Y-m-d H:i:s",$value["process_time"])?0:0;
+			$apply[$key]["pre_tax"] = $value["pre_tax"]?0:0;
+			$apply[$key]["after_tax"] = $value["after_tax"]?0:0;
+			$apply[$key]["fee"] = $value["fee"]?0:0;
+			$apply[$key]["amount_payable"] = $value["amount_payable"]?0:0;
 		}
 		$this		->assign("apply",$apply);
     	$this		->display();
+	}
+	public function withdrawDo(){
+		$id=(int)($_GET["id"]);
+		
 	}
 	// 结算明细
 	public function income(){
@@ -69,7 +76,11 @@ class FinanceAction extends CommonAction {
 				$where = $where." and uid =".$uid; 
 			}
 		}else{ // 按业绩状态查询
-			$where = "performance_status = ".$_GET["status"];
+			if($_GET["status"]=="all"){
+				$where = "1";
+			}else{
+				$where = "performance_status = ".$_GET["status"];
+			}	
 		}
 		$in		= M("income");
 		$income = $in->where($where)->select();
@@ -118,7 +129,272 @@ class FinanceAction extends CommonAction {
 		$this->assign("income",$income);
 		$this->display();
 	}
+	//结算明细表操作_冻结
 	public function incomeDo(){
-	
+		$id		= (int)($_GET["id"]);
+		$data["frozen_status"]=0;
+		$in 	= M("income");
+		$in		->where("id=".$id)->data($data)->save(); //冻结成功，改成冻结状态
+		$this	->success("冻结成功！","SITE_URL/?m=Finance&a=income");
+	}
+	//申请提现状态报表
+	public function withdrawListRule(){
+		$with					= M("withdraw_list_rule");
+		$withdraw				= $with->where("id=1")->select();
+		$this					->assign("withdraw",$withdraw);
+		$this					->display();
+	}
+	//订制申请提现状态报表
+	public function withdrawListRuleDo(){
+		$data["uid"]			= (int)($_POST["uid"]);
+		$data["apply_date"]		= (int)($_POST["timestmp"]);
+		$data["pre_tax"]		= (int)($_POST["income_zong"]);
+		$data["after_tax"]		= (int)($_POST["income_shuihou"]);
+		$data["fee"]			= (int)($_POST["income_shouxufei"]);
+		$data["amount_payable"]	= (int)($_POST["income_yingfu"]);
+		$data["process_time"]	= (int)($_POST["pay_time"]);
+		$data["status"]			= (int)($_POST["status"]);
+		$with					= M("withdraw_list_rule");
+		$withdraw				= $with->where("id=1")->data($data)->save(); //修改各字段的状态
+		$this->success("订制申请提现报表成功","SITE_URL/?m=Finance&a=index");
+	}
+	// 导出申请提现报表
+	public function withdrawExport(){
+		//输出的文件类型为excel
+		header("Content-type:application/vnd.ms-excel");
+		//提示下载
+		header("Content-Disposition:attachement;filename=提现申请列表_".date("Y-m-d").".xls");
+		//查询提现申请表
+		$finance_apply	= M("finance_apply");
+		$finance 	  	= $finance_apply->select();
+		//查询申请提现状态报表
+		$with			= M("withdraw_list_rule");
+		$withdraw		= $with->where("id=1")->select();
+		$ReportArr	  	= array();
+		//将关系数组转换成索引数组
+		foreach($finance as $key =>$val){
+			if($withdraw[0]["uid"]==0){
+				$ReportArr[$key][]=$val["uid"]; //会员ID
+			}
+			if($withdraw[0]["apply_date"]==0){
+				$ReportArr[$key][]=date("Y-m-d H:i:s",$val["apply_date"]); //申请提现时间
+			}
+			if($withdraw[0]["pre_tax"]==0){
+				$ReportArr[$key][]=$val["pre_tax"]?0:0; //税前总收入
+			}
+			if($withdraw[0]["after_tax"]==0){
+				$ReportArr[$key][]=$val["after_tax"]?0:0; //税后总收入
+			}
+			if($withdraw[0]["fee"]==0){
+				$ReportArr[$key][]=$val["fee"]?0:0; //手续费
+			}
+			if($withdraw[0]["amount_payable"]==0){
+				$ReportArr[$key][]=$val["amount_payable"]?0:0; //应付金额
+			}
+			if($withdraw[0]["process_time"]==0){
+				$ReportArr[$key][]=date("Y-m-d H:i:s",$val["process_time"])?0:0; //操作时间
+			}
+			if($withdraw[0]["status"]==0){
+				if($val["status"]==1){ // 支付状态
+					$ReportArr[$key][]="未支付";
+				}else if($val["status"]==2){
+					$ReportArr[$key][]="已支付";
+				}else{
+					$ReportArr[$key][]="异常";
+				}
+			}
+		}
+		//报表数据
+		$ReportContent = '';
+		$num1 = count($ReportArr);
+		for($i=0;$i<$num1;$i++){
+			$num2 = count($ReportArr[$i]);
+			for($j=0;$j<$num2;$j++){
+				//ecxel都是一格一格的，用\t将每一行的数据连接起来 \t制表符
+				$ReportContent .= '"'.$ReportArr[$i][$j].'"'."\t";
+			}
+			//最后连接\n 表示换行
+			$ReportContent .= "\n";
+		}
+		//用的utf-8 最后转换一个编码为gb
+		//$ReportContent = mb_convert_encoding($ReportContent,"gb2312","utf-8");
+		//需要查询出来的字段信息
+		if($withdraw[0]["uid"]==0){ //判断是否要导出会员ID信息
+			$t[]="会员ID";
+		}
+		if($withdraw[0]["apply_date"]==0){ //判断是否要导出申请时间信息
+			$t[]="申请时间";
+		}
+		if($withdraw[0]["pre_tax"]==0){ //判断是否要导出税前总收入信息
+			$t[]="税前总收入(￥)";
+		}
+		if($withdraw[0]["after_tax"]==0){ //判断是否要导出税后总收入信息
+			$t[]="税后总收入(￥)";
+		}
+		if($withdraw[0]["fee"]==0){ //判断是否要导出手续费信息
+			$t[]="手续费(￥)";
+		}
+		if($withdraw[0]["amount_payable"]==0){ //判断是否要导出应付金额信息
+			$t[]="应付金额(￥)";
+		}
+		if($withdraw[0]["process_time"]==0){ //判断是否要导出处理时间信息
+			$t[]="处理时间";
+		}
+		if($withdraw[0]["status"]==0){ //判断是否要导出支付状态信息
+			$t[]="支付状态";
+		}
+		for($k=0;$k<count($t);$k++){
+			//ecxel都是一格一格的，用\t将每一行的数据连接起来 \t制表符
+			$ReportTitle .= '"'.$t[$k].'"'."\t";
+		}
+		//输出即提示下载
+		echo $ReportTitle."\n".$ReportContent;
+	}
+	// 订制结算明细报表
+	public function reportIncomeListRule(){
+		$report_income			= M("report_income_list_rule");
+		$report					= $report_income->where("id=1")->select();
+		$this					->assign("report",$report);
+		$this					->display();
+	}
+	// 订制结算明细报表
+	public function reportIncomeListRuleDo(){
+		$data["pid"]			= (int)($_POST["plan_id"]);
+		$data["uid"]			= (int)($_POST["uid"]);
+		$data["settlement_time"]= (int)($_POST["times"]);
+		$data["real_income"]	= (int)($_POST["income_real"]);
+		$data["settlement_income"]= (int)($_POST["income"]);
+		$data["ip"]				= (int)($_POST["count_ip"]);
+		$data["pv"]				= (int)($_POST["count_pv"]);
+		$data["cps"]			= (int)($_POST["count_cps"]);
+		$data["cpa"]			= (int)($_POST["count_cpa"]);
+		$data["performance_status"]= (int)($_POST["status"]);
+		$data["settlement_status"]= (int)($_POST["pay_status"]);
+		$data["status"]			= (int)($_POST["status"]);
+		$report_income			= M("report_income_list_rule");
+		$report					= $report_income->where("id=1")->data($data)->save(); //修改各字段的状态
+		$this->success("订制结算明细报表成功","SITE_URL/?m=Finance&a=income");
+	}
+	// 导出结算明细报表
+	public function incomeExport(){
+		//输出的文件类型为excel
+		header("Content-type:application/vnd.ms-excel");
+		//提示下载
+		header("Content-Disposition:attachement;filename=结算明细列表_".date("Y-m-d").".xls");
+		//查询结算明细表
+		$in				= M("income");
+		$income 	  	= $in->select();
+		//查询结算明细状态报表
+		$report_income	= M("report_income_list_rule");
+		$report			= $report_income->where("id=1")->select();
+		$ReportArr	  	= array();
+		//将关系数组转换成索引数组
+		foreach($income as $key =>$val){
+			if($report[0]["pid"]==0){
+				$ReportArr[$key][]=$val["pid"]; //计划ID
+			}
+			if($report[0]["uid"]==0){
+				$ReportArr[$key][]=$val["uid"]; //网站主ID
+			}
+			if($report[0]["settlement_time"]==0){
+				$ReportArr[$key][]=date("Y-m-d H:i:s",$val["settlement_time"]); //结算日期
+			}
+			if($report[0]["real_income"]==0){
+				$ReportArr[$key][]=$val["real_income"]?0:0; //真实收入
+			}
+			if($report[0]["settlement_income"]==0){
+				$ReportArr[$key][]=$val["settlement_income"]?0:0; //结算收入
+			}
+			if($report[0]["ip"]==0){
+				$ReportArr[$key][]=$val["ip"]?0:0; //IP
+			}
+			if($report[0]["pv"]==0){
+				$ReportArr[$key][]=$val["pv"]?0:0; //PV
+			}
+			if($report[0]["click"]==0){
+				$ReportArr[$key][]=$val["click"]?0:0; //CLICK
+			}
+			if($report[0]["cps"]==0){
+				$ReportArr[$key][]=$val["cps"]?0:0; //CPS
+			}
+			if($report[0]["cpa"]==0){
+				$ReportArr[$key][]=$val["cpa"]?0:0; //CPA
+			}
+			if($report[0]["performance_status"]==0){
+				if($val["performance_status"]==1){ //业绩状态
+					$ReportArr[$key][]="未确认";
+				}else if($val["performance_status"]==2){
+					$ReportArr[$key][]="已确认";
+				}else{
+					$ReportArr[$key][]="无效";
+				}
+			}
+			if($report[0]["settlement_status"]==0){
+				if($val["settlement_status"]==1){ //结算状态
+					$ReportArr[$key][]="未支付";
+				}else if($val["settlement_status"]==2){
+					$ReportArr[$key][]="已支付";
+				}else{
+					$ReportArr[$key][]="异常";
+				}
+			}
+		}
+		//报表数据
+		$ReportContent = '';
+		$num1 = count($ReportArr);
+		for($i=0;$i<$num1;$i++){
+			$num2 = count($ReportArr[$i]);
+			for($j=0;$j<$num2;$j++){
+				//ecxel都是一格一格的，用\t将每一行的数据连接起来 \t制表符
+				$ReportContent .= '"'.$ReportArr[$i][$j].'"'."\t";
+			}
+			//最后连接\n 表示换行
+			$ReportContent .= "\n";
+		}
+		//用的utf-8 最后转换一个编码为gb
+		//$ReportContent = mb_convert_encoding($ReportContent,"gb2312","utf-8");
+		//需要查询出来的字段信息
+		if($report[0]["pid"]==0){ //判断是否要导出计划ID信息
+			$t[]="计划ID";
+		}
+		if($report[0]["uid"]==0){ //判断是否要导出网站主ID信息
+			$t[]="网站主ID";
+		}
+		if($report[0]["settlement_time"]==0){ //判断是否要导出结算时间信息
+			$t[]="结算时间";
+		}
+		if($report[0]["real_income"]==0){ //判断是否要导出真实收入信息
+			$t[]="真实收入";
+		}
+		if($report[0]["settlement_income"]==0){ //判断是否要导出结算收入信息
+			$t[]="结算收入";
+		}
+		if($report[0]["ip"]==0){ //判断是否要导出IP信息
+			$t[]="IP";
+		}
+		if($report[0]["pv"]==0){ //判断是否要导出PV信息
+			$t[]="PV";
+		}
+		if($report[0]["click"]==0){ //判断是否要导出CLICK信息
+			$t[]="CLICK";
+		}
+		if($report[0]["cps"]==0){ //判断是否要导出CPS信息
+			$t[]="CPS";
+		}
+		if($report[0]["cpa"]==0){ //判断是否要导出CPA信息
+			$t[]="CPA";
+		}
+		if($report[0]["performance_status"]==0){ //判断是否要导出业绩状态信息
+			$t[]="业绩状态";
+		}
+		if($report[0]["settlement_status"]==0){ //判断是否要导出支付状态信息
+			$t[]="支付状态";
+		}
+		for($k=0;$k<count($t);$k++){
+			//ecxel都是一格一格的，用\t将每一行的数据连接起来 \t制表符
+			$ReportTitle .= '"'.$t[$k].'"'."\t";
+		}
+		//输出即提示下载
+		echo $ReportTitle."\n".$ReportContent;
 	}
 }
--- Code/App/Web/Lib/Action/FinanceWebAction.class.php
+++ Code/App/Web/Lib/Action/FinanceWebAction.class.php
@@ -16,7 +16,6 @@
 class FinanceWebAction extends CommonAction {
 	// 提现明细
     public function index(){
-		// 创建提现明细表对象
 		$fi=M("finance");
 		// 按状态检索
 		$status=(int)($_POST["status"]);
@@ -25,8 +24,9 @@ class FinanceWebAction extends CommonAction {
 		}else{
 			$finance=$fi->where("payment_status =".$status)->select();
 		}
+		// 处理提现时间，支付时间，支付状态
 		foreach($finance as $key =>$val){
-			$finance[$key]["withdrawal_date"] = date("Y-m-d",$val["withdrawal_date"]);
+			$finance[$key]["withdraw_date"] = date("Y-m-d",$val["withdraw_date"]);
 			$finance[$key]["paid_time"] = date("Y-m-d",$val["paid_time"]);
 			switch($val["payment_status"]){
 				case 1:
@@ -46,17 +46,110 @@ class FinanceWebAction extends CommonAction {
 		$this->assign("finance",$finance);
 		$this->display();
     }
+	// 申请提现
 	public function financePay(){
-		$this->assign("title","申请提现");
+		$this	->assign("title","申请提现");
+		$uid 	= $_SESSION[C('USER_AUTH_KEY')];
+		// 创建web_balance表对象
+		$web  	= M("web_balance");
+		$balance= $web->where("uid=".$uid)->select();
+		// 创建member_detail表对象
+		$me  	= M("member_detail");
+		$member	= $me->where("uid=".$uid)->select();
+		$apply_date= date("Y-m-d H:i:s",time());
+		$this	->assign("balance",$balance);
+		$this	->assign("member",$member);
+		$this	->assign("apply_date",$apply_date);
+		$this	->display();
+    }
+	// 处理提现申请数据
+	public function financePayDo(){
+		$uid						= $_SESSION[C('USER_AUTH_KEY')];
+		$data["uid"] 				= $uid;
+		$data["apply_date"] 		= time();
+		$data["withdraw_balance"] 	= $_POST["balance"];
+		$data["withdraw_auto"]		= $_POST["withdraw_auto"];
+		$finance_apply	  			= M("finance_apply");
+		$finance		  			= $finance_apply->where("uid=".$uid)->data($data)->add(); // 添加提现申请数据
+		$web_balance	  			= M("web_balance");
+		$web_balance				->where('uid='.$uid)->setInc('withdraw_num',1);  // 提现次数+1
+		$this						->success("提现申请提交成功","SITE_URL/?m=FinanceWeb&a=financePay"); 
+		// 成功跳转提交明细页面
+	}
+	// 收款人银行卡信息
+	public function financeBank(){
+		$this	->assign("title","银行信息");
+		$uid 	= $_SESSION[C('USER_AUTH_KEY')];
+		$me  	= M("member_detail");
+		$member	= $me->where("uid=".$uid)->select();
+		$this	->assign("member",$member);
+		$this	->display();
+    }
+	// 编辑银行信息
+	public function financeBankEdit(){
+		$this->assign("title","修改银行信息");
+		$uid=$_SESSION[C('USER_AUTH_KEY')];
+		$me=M("member");
+		$member=$me->where("id =".$uid)->select();
+		$this->assign("member",$member);
+		$med=M("member_detail");
+		$memberDetail=$med->where("uid=".$uid)->select();
+		$this->assign("memberDetail",$memberDetail);
+		$this->display();
+    }
+	// 处理银行信息数据
+	public function financeBankEditDo(){
+		$this			->assign("title","修改银行信息");
+		$real_name 		= $_POST["account"]; // 开户人、收款人
+		$legal_status 	= $_POST["legal_status"]; // 法人状态：个人、企业
+		$bank_short 	= $_POST["bank"]; // 银行简称
+		$bank_name 		= $_POST["bank_name"]; // 银行全称
+		$card_number 	= $_POST["accounts"]; // 开户账号
+		$accounts_again = $_POST["accounts_again"]; // 再次输入账号
+		// 更改member表信息
+		$data_member["real_name"]= $real_name;
+		$data_member["legal_status"]= $legal_status;
+		// 更改member_detail表信息 
+		$data_member_detail["bank_short"]= $bank_short;
+		$data_member_detail["card_author"]= $real_name;
+		$data_member_detail["bank_name"]= $bank_name;
+		$data_member_detail["card_number"]= $card_number;
+		// PHP判断两次账号输入是否相同
+		if($card_number!=$accounts_again){
+			$this->error("两次银行卡卡号或存折账号 输入不一致!","SITE_URL/?m=FinanceWeb&a=financeBankEdit"); // 失败返回重新编辑
+		}else{
+			$uid=$_SESSION[C('USER_AUTH_KEY')]; // 获取会员id
+			$me=M("member");
+			$member=$me->where("id =".$uid)->data($data_member)->save(); // 更改真实姓名、法人信息
+			$med=M("member_detail");
+			$memberDetail=$med->where("uid=".$uid)->data($data_member_detail)->save(); // 更改收款人银行卡信息
+			$this->success("数据更改成功","SITE_URL/?m=FinanceWeb&a=financeBank"); // 成功则跳转银行信息页面
+		}
 		
+	}
+	// 结算明细
+	public function financeDetail(){
+		// 按结算时间所处时间段查询
+		$start	=	$_POST["time_start"];
+		$end	=	$_POST["time_end"];
+		$start	=	strtotime($start);
+		$end	=	strtotime($end);
+		$in	= M("income");
+		if(empty($start)&&empty($end)){
+			$income=$in->select(); // 默认
+		}else if(empty($start)&&!empty($end)){
+			$income=$in->where("settlement_time <= ".$end)->select(); // 早于结束时间
+		}else if(!empty($start)&&empty($end)){
+			$income=$in->where("settlement_time >= ".$start)->select(); // 晚于开始时间
+		}else{
+			$income=$in->where("settlement_time >= ".$start." and settlement_time <= ".$end)->select(); // 介于开始结束时间之间
+		}
+		foreach($income as $key =>$val){
+			$income[$key]["start_date"] = date("Y-m-d",$val["start_date"]); // 周期开始时间
+			$income[$key]["end_date"] = date("Y-m-d",$val["end_date"]); // 周期结束时间
+			$income[$key]["settlement_time"] = date("Y-m-d",$val["settlement_time"]); // 结算时间
+		}
+		$this->assign("income",$income);
 		$this->display();
-    }
-	public function money_detail(){
-		$this->assign("title","结算明细");
-		$this->display();
-    }
-	public function money_bank(){
-		$this->assign("title","银行信息");
-		$this->display();
-    }
+	}
 }
