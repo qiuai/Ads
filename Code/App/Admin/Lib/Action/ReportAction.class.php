b02898265f3f462743e9b8cb4b80dd6bae2746ee
Vonwey <vonwey@qq.com>
2013-12-24 11:49:32 +0800

    同步

--- Code/App/Admin/Lib/Action/ReportAction.class.php
+++ Code/App/Admin/Lib/Action/ReportAction.class.php
@@ -22,7 +22,7 @@ class ReportAction extends CommonAction {
 	 */
 	public function webReport(){
 		// 网站主UID
-		$uid = $_REQUEST['uid'] ? $_REQUEST['uid'] : 0;
+		$uid = $_REQUEST['uid'] ? $_REQUEST['uid'] : '';
 		
 		// 搜索UID
 		if($uid){
@@ -31,8 +31,8 @@ class ReportAction extends CommonAction {
 		
 		// 搜索日期
 		if(intval($_REQUEST['start_date']) && intval($_REQUEST['end_date'])){
-			$where['start_date'] = array('gt', intval($_REQUEST['start_date']));
-			$where['end_date'] = array('lt', intval($_REQUEST['end_date']));
+			$where['start_date'] = array('gt', strtotime(intval($_REQUEST['start_date'])));
+			$where['end_date'] = array('lt', strtotime(intval($_REQUEST['end_date'])));
 		}
 		
 		// 列表数据
@@ -43,6 +43,8 @@ class ReportAction extends CommonAction {
 // 		$income = $model->where($where)->find();
 // 		var_dump($income);
 		$this->assign('uid', $uid);
+		$this->assign('start_date', $_REQUEST['start_date']);
+		$this->assign('end_date', $_REQUEST['end_date']);
 		$this->display();
 	}
 	/**
@@ -52,18 +54,18 @@ class ReportAction extends CommonAction {
 	 * @CreateDate: 2013-12-23 下午9:31:09
 	 */
 	public function planReport(){
-		// 网站主UID
-		$pid = $_REQUEST['pid'] ? $_REQUEST['pid'] : 0;
+		// 计划PID
+		$pid = $_REQUEST['pid'] ? $_REQUEST['pid'] : '';
 	
-		// 搜索UID
+		// 搜索PID
 		if($pid){
 			$where['pid'] = intval($pid);
 		}
 	
 		// 搜索日期
 		if(intval($_REQUEST['start_date']) && intval($_REQUEST['end_date'])){
-			$where['start_date'] = array('gt', intval($_REQUEST['start_date']));
-			$where['end_date'] = array('lt', intval($_REQUEST['end_date']));
+			$where['start_date'] = array('gt', strtotime(intval($_REQUEST['start_date'])));
+			$where['end_date'] = array('lt', strtotime(intval($_REQUEST['end_date'])));
 		}
 	
 		// 列表数据
@@ -71,9 +73,11 @@ class ReportAction extends CommonAction {
 	
 		// 结算时间降序
 		$income = $this->memberPage($model, $where, 10, 'settlement_time desc');
-// 				$income = $model->where($where)->find();
+// 		echo $model->getLastSql();
 // 				var_dump($income);
 		$this->assign('pid', $pid);
+		$this->assign('start_date', $_REQUEST['start_date']);
+		$this->assign('end_date', $_REQUEST['end_date']);
 		$this->display();
 	}
 }
