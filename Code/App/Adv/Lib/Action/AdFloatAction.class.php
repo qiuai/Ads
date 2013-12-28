7ec471c1668384718933b23e9b300874e5332f90
Vonwey <vonwey@qq.com>
2013-12-28 16:35:00 +0800

    ad

---
+++ Code/App/Adv/Lib/Action/AdFloatAction.class.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 广告联盟系统  广告服务
+ * 
+ * @copyright (C)2012 ZHTS Inc.
+ * @project project_name
+ * @author Vonwey <VonweyWang@gmail.com>
+ * @CreateDate: 2013-11-25 上午10:07:57
+ * @version 1.0
+ *
+ * @ModificationHistory  
+ * Who          When                What 
+ * --------     ----------          ------------------------------------------------ 
+ * Vonwey   2013-11-25 上午10:07:57      todo
+ */
+class AdServiceAction extends Action {
+	/**
+	 * 初始化
+	 *
+	 * @author Vonwey <VonweyWang@gmail.com>
+	 * @CreateDate: 2013-12-28 下午4:28:47
+	 */
+	function _initialize(){
+		
+	}
+}
