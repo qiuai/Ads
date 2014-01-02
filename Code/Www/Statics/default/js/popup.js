14fe7dd1fc1c0b18afd8230746492e561a5a09a9
Vonwey <vonwey@qq.com>
2014-01-02 18:40:03 +0800

    提交

--- Code/Www/Statics/default/js/popup.js
+++ Code/Www/Statics/default/js/popup.js
@@ -1,5 +1,3 @@
-var w=1024;
-var h=768;
 /*设置弹出窗口样式*/
 for(var i=0;i<adUrl.length;i++){
 	var adPopup = window.open(adUrl[i], "", "width="+(window.screen.availWidth-10)+", height="+(window.screen.availHeight-30)+ " ,top=0, left=0, toolbar=yes, menubar=yes, scrollbars=yes, resizable=yes, location=yes, status=yes"); 			
