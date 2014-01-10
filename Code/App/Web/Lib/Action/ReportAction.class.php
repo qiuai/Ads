6ec3ceeb2048adde23594590fe58ae8da7906413
Vonwey <vonwey@qq.com>
2014-01-10 16:05:38 +0800

    提价

--- Code/App/Web/Lib/Action/ReportAction.class.php
+++ Code/App/Web/Lib/Action/ReportAction.class.php
@@ -35,7 +35,7 @@ class ReportAction extends CommonAction {
      */
 	 public function cpcReport(){ 
 	 	// 生成报表
-	 	$this->tenDaysBefore();
+	 	$this->tenDaysBefore(2);
 	 	
 		$this->assign("location","CPC明细报表");
 		$this->display('index');
@@ -48,7 +48,7 @@ class ReportAction extends CommonAction {
      */
 	 public function cpmReport(){ 
 	 	// 生成报表
-	 	$this->tenDaysBefore();
+	 	$this->tenDaysBefore(1);
 	 	
 		$this->assign("location","CPM明细报表");
 		$this->display('index');
@@ -61,19 +61,24 @@ class ReportAction extends CommonAction {
 	 * @author Vonwey <VonweyWang@gmail.com>
 	 * @CreateDate: 2013-12-20 下午1:45:06
 	 */
-	public function tenDaysBefore(){
+	public function tenDaysBefore($pay_type=0){
 		// 会员报表
 		if($_SESSION[C('WEB_AUTH_KEY')]){
-			$where = " and uid = ". intval($_SESSION[C('WEB_AUTH_KEY')]);
+			$where = " and z.uid = ". intval($_SESSION[C('WEB_AUTH_KEY')]);
 		}else{
 			return;
 		}
 		
-		$model = M('Income');
+		$model = M();
+		
+		// 计费方式
+		if($pay_type){
+		    $where .= " and p.pay_type = $pay_type ";
+		}
 		
 		// 搜索代码位
 		if(intval($_REQUEST['zone_id'])){
-			$where .= " and i.zid = " . intval($_REQUEST['zone_id']);
+			$where .= " and zv.zid = " . intval($_REQUEST['zone_id']);
 		}
 		
 		// 搜索日期
@@ -81,21 +86,31 @@ class ReportAction extends CommonAction {
 			$start_date = strtotime($_REQUEST['start_date']);
 			$end_date = strtotime($_REQUEST['end_date']);
 			
-			$where .= " and i.start_date >= $start_date";
-			$where .= " and i.end_date < $end_date";
+			$where .= " and zv.day_start_time >= $start_date";
+			$where .= " and zv.day_start_time < $end_date";
 			
 			while($start_date <= $end_date && $start_date < strtotime(date('Ymd',time()))){
 				$day = mktime(0,0,0,date("m",$start_date) ,date('d',$start_date)-1,date("Y",$start_date));
-				$data = $model->query("select sum(click) as click, sum(pv) as pv, sum(cpm) as cpm, sum(cpc) as cpc, sum(real_income) as income, sum(ip) as ip from " . C('DB_PREFIX') . "income i where settlement_time < $start_date and settlement_time >= $day $where");
-				if($data[0]['ip']){
-					foreach($data[0] as $key=>$value){
-						$data[0][$key] = $value ? $value : 0;
-						$sum[$key] = intval($value) + intval($sum[$key]);
-					}
-					$data[0]['day'] = date('Ymd', $start_date);
-						
-					$list[] = $data[0];
+				$data = $model->query("select zv.click_ip_num as click, zv.view_pv_num as pv, p.site_master_pay_price as price, zv.view_pv_num as ip from " . C('DB_PREFIX') . "zone_visit_count zv join " . C('DB_PREFIX') . "zone z on z.id = zv.zid join " . C('DB_PREFIX') . "ad_plan p on p.id = zv.pid where zv.day_start_time < $start_date and zv.day_start_time >= $day $where");
+				
+				$report = array();
+				$report['day'] = date('Ymd', $start_date);
+				
+				if($data){
+				    foreach($data as $k=>$v){
+				        $report['income'] = $data[$k]['price'] * $data[$k]['ip'] + $report['income'];	// 预计收入
+				        $report['click'] = $v['click'] + $report['click'];
+				        $report['pv'] = $v['pv'] + $report['pv'];
+				        $report['ip'] = $v['ip'] + $report['ip'];
+				    }
+				}else{
+				    $report['income'] = 0;	// 预计收入
+				    $report['click'] = 0;
+				    $report['pv'] = 0;
+				    $report['ip'] = 0;
 				}
+				$list[] = $report;
+				
 				$start_date = mktime(0,0,0,date("m",$start_date) ,date('d',$start_date)+1,date("Y",$start_date));
 			}
 			
@@ -117,18 +132,35 @@ class ReportAction extends CommonAction {
 			for($i=6; $i>0; $i--){
 				$day = mktime(0,0,0,date("m") ,(date('d',$today)-($i+1)),date("Y"));
 				$yestoday = mktime(0,0,0,date("m",$today) ,(date('d',$today)-$i),date("Y"));
-				$data = $model->query("select sum(click) as click, sum(pv) as pv, sum(cpm) as cpm, sum(cpc) as cpc, sum(real_income) as income, count(ip) as ip from " . C('DB_PREFIX') . "income i where settlement_time < $yestoday and settlement_time >= $day $where");
-					
-				foreach($data[0] as $key=>$value){
-					$data[0][$key] = $value ? $value : 0;
-					$sum[$key] = intval($value) + intval($sum[$key]);
+				$data = $model->query("select zv.click_ip_num as click, zv.view_pv_num as pv, p.site_master_pay_price as price, zv.view_pv_num as ip from " . C('DB_PREFIX') . "zone_visit_count zv join " . C('DB_PREFIX') . "zone z on z.id = zv.zid join " . C('DB_PREFIX') . "ad_plan p on p.id = zv.pid where zv.day_start_time < $yestoday and zv.day_start_time >= $day $where");
+
+				$report = array();
+				$report['day'] = date('Ymd', $yestoday);
+				
+				if($data){
+				    foreach($data as $k=>$v){
+				        $report['income'] = $data[$k]['price'] * $data[$k]['ip'] + $report['income'];	// 预计收入
+				        $report['click'] = $v['click'] + $report['click'];
+				        $report['pv'] = $v['pv'] + $report['pv'];
+				        $report['ip'] = $v['ip'] + $report['ip'];
+				    }
+				}else{
+				    $report['income'] = 0;	// 预计收入
+				    $report['click'] = 0;
+				    $report['pv'] = 0;
+				    $report['ip'] = 0;
 				}
-				$data[0]['day'] = date('Ymd', $yestoday);
-					
-				$list[] = $data[0];
+				$list[] = $report;
 			}
 		}
-
+		
+		foreach ($list as $key=>$value){
+		    $sum['income'] = $value['income'] + $sum['income'];
+		    $sum['click'] = $value['click'] + $sum['click'];
+		    $sum['pv'] = $value['pv'] + $sum['pv'];
+		    $sum['ip'] = $value['ip'] + $sum['ip'];
+		}
+		 
 		// 总计
 		$sum['day'] = '合计';
 		
